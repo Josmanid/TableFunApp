bb035e5a4fb58547fa40502661bb4ed3b01c40fc
Jonas Martin Smidt <jonassmidt@hotmail.dk>
2026-08-19 15:52:03 +0200

    Update CueViewModel

--- app/src/main/java/com/example/tablefunapp/ui/CueViewModel.kt
+++ app/src/main/java/com/example/tablefunapp/ui/CueViewModel.kt
@@ -1,20 +1,36 @@
 package com.example.tablefunapp.ui
 
+import android.app.Application
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import com.example.tablefunapp.models.Cue
 import com.example.tablefunapp.repository.CueRepository
+import com.example.tablefunapp.repository.SoundRepository
 
-class CueViewModel : ViewModel() {
+class CueViewModel(application: Application) : AndroidViewModel(application) {
 
     //Dependency
-    private val repository = CueRepository()
+    private val cueRepository = CueRepository()
+    private val soundRepository = SoundRepository(application)
 
     //States to hold
     val cues = mutableStateOf<List<Cue>>(emptyList())
 
     init {
-        cues.value = repository.getCues()
+        cues.value = cueRepository.getCues()
+    }
+
+    fun playShort(cue: Cue) {
+        soundRepository.play(cue.shortSound)
+    }
+
+    fun playLong(cue: Cue) {
+        soundRepository.play(cue.longSound)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        soundRepository.stop()
     }
 
 }
